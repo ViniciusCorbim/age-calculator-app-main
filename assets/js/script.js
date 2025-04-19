576b14f690fea3c0eb4fa9874331b155108646bf
Vinicius Corbim <viniciuscorbim0@gmail.com>
2025-04-18 21:09:36 -0300

    Optimized JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-//-----------------input and span.error-----------------
+//-----------------input and span.error-----------------\\
 const day_input = document.getElementById("day_input");
 const day_span = document.getElementById("day_error");
 
@@ -9,79 +9,52 @@ const year_input = document.getElementById("year_input");
 const year_span = document.getElementById("year_error");
 year_input.max = new Date().getFullYear();
 
-//------------------------output spans------------------------
+//------------------------output spans------------------------\\
 const years_output = document.getElementById("years_output");
 const months_output = document.getElementById("months_output");
 const days_output = document.getElementById("days_output");
 
 //-----------------------------------anonymous functions-----------------------------------\\
-const printMessage = (input, span, t) => {
-  if (input.validity.valueMissing) {
-    span.innerText = "This field is required";
-  } else if (input.validity.rangeOverflow || input.validity.rangeUnderflow) {
+const printErrorMessage = (input, span, t) => {
+  span.innerText = "";
+
+  if (input.validity.valueMissing) span.innerText = "This field is required";
+  if (input.validity.rangeOverflow || input.validity.rangeUnderflow)
     span.innerText = "Must be a valid " + t;
-  } else if (checkYear(input) && t == "year") {
+  if (input.validity.rangeOverflow && t == "year")
     span.innerText = "Must be in the past";
-  } else {
-    span.innerText = "";
-  }
 };
 
-const checkYear = (input) => {
-  const check =
-    Number.parseInt(input.value) > Number.parseInt(new Date().getFullYear());
-
-  return check ? true : false;
-};
-
-//button btn_calc
-const btn = document.getElementById("btn_calc");
-btn.addEventListener("click", () => {
-  printMessage(day_input, day_span, "day");
-  printMessage(month_input, month_span, "month");
-  printMessage(year_input, year_span, "year");
+const checkInputs = () => {
+  printErrorMessage(day_input, day_span, "day");
+  printErrorMessage(month_input, month_span, "month");
+  printErrorMessage(year_input, year_span, "year");
 
   const validation =
     !day_input.checkValidity() ||
     !month_input.checkValidity() ||
     !year_input.checkValidity();
-  if (validation) {
-    document.getElementById("input_section").classList.toggle("error", true);
-    return;
-  }
 
-  const birthday = new Date();
-  birthday.setFullYear(year_input.value);
-  birthday.setMonth(month_input.value - 1);
-  birthday.setDate(day_input.value);
+  return validation;
+};
 
-  birthday.setHours(0);
-  birthday.setMinutes(0);
-  birthday.setSeconds(0);
+const checkDate = (date) => {
+  const validation =
+    date.getDate() != day_input.value ||
+    date.getMonth() + 1 != month_input.value ||
+    date.getFullYear() != year_input.value;
 
-  if (
-    birthday.getDate() != day_input.value ||
-    birthday.getMonth() + 1 != month_input.value ||
-    birthday.getFullYear() != year_input.value
-  ) {
-    document.getElementById("input_section").classList.toggle("error", true);
-    day_span.innerHTML = "Must be a valid date";
-    return;
-  }
+  return validation;
+};
 
-  document.getElementById("input_section").classList.toggle("error", false);
-  const calce = calc(birthday);
-  printOutput(calce);
-});
-
-const calc = (date) => {
+const toCalc = (date) => {
   const today = new Date();
 
   //05 08 2004 - 18 04 2025 = 18 days
   //05 08 2004 - 04 08 2025 = 04 days
   //05 08 2004 - 05 08 2025 = 00 days
   //05 08 2004 - 20 08 2025 = 15 days
-  let days =
+  let day =
     today.getMonth() == date.getMonth()
       ? today.getDate() < date.getDate()
         ? today.getDate()
@@ -91,35 +64,57 @@ const calc = (date) => {
   //05 08 2004 - 18 04 2025 = 8 months
   //05 08 2004 - 24 08 2025 = 0 months
   //05 08 2004 - 25 12 2025 = 4 months
-  let months =
+  let month =
     today.getMonth() >= date.getMonth()
       ? today.getMonth() - date.getMonth()
       : 12 - date.getMonth() + today.getMonth();
 
-  let years = today.getFullYear() - date.getFullYear();
+  let year = today.getFullYear() - date.getFullYear();
 
   //05 08 2004 - 18 04 2025 = 20 years
   //05 08 2004 - 04 08 2025 = 20 years
   //05 08 2004 - 05 08 2025 = 21 years
   //05 08 2004 - 25 12 2025 = 21 years
-  years =
+  year =
     today.getMonth() > date.getMonth()
-      ? years
+      ? year
       : today.getMonth() == date.getMonth()
       ? today.getDate() < date.getDate()
-        ? years - 1
-        : years
-      : years - 1;
+        ? year - 1
+        : year
+      : year - 1;
 
   return {
-    year: years,
-    month: months,
-    day: days,
+    years: year,
+    months: month,
+    days: day,
   };
 };
 
-const printOutput = (out) => {
-  years_output.innerHTML = out.year;
-  months_output.innerHTML = out.month;
-  days_output.innerHTML = out.day;
-};
+//-----------------------button btn_calc-----------------------\\
+const btn = document.getElementById("btn_calc");
+btn.addEventListener("click", () => {
+  const input_section = document.getElementById("input_section");
+  input_section.classList.toggle("error", true);
+
+  if (checkInputs()) return;
+  const birthday = new Date();
+  birthday.setFullYear(year_input.value);
+  birthday.setMonth(month_input.value - 1);
+  birthday.setDate(day_input.value);
+
+  birthday.setHours(0);
+  birthday.setMinutes(0);
+  birthday.setSeconds(0);
+
+  if (checkDate(birthday)) {
+    day_span.innerHTML = "Must be a valid date";
+    return;
+  }
+  input_section.classList.toggle("error", false);
+
+  const calc = toCalc(birthday);
+  years_output.innerHTML = calc.years;
+  months_output.innerHTML = calc.months;
+  days_output.innerHTML = calc.days;
+});
